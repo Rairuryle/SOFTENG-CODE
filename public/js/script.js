50b32612bc1d7124699acb02aed8c058d856efa1
Rairuryle <104247460+Rairuryle@users.noreply.github.com>
2023-10-26 17:22:48 +0800

    tbl test

--- public/js/script.js
+++ public/js/script.js
@@ -266,3 +266,15 @@ $(function() {
     });
   });
   
+
+document.AddEventListener('DOMContentLoaded', function () {
+    loadHTMLTable([]);
+});
+
+function loadHTMLTable(data) {
+    const table = document.querySelector('table tbody');
+    let tableHTML = "";
+    if (data.length === 0) {
+        table.innerHTML = "<tr><td class='no-data' colspan='5'>No data</td></tr>";
+    }
+}
